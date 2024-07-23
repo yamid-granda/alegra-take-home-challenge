50868482472a16753f4f2443c085efada9920d31
yamid-granda <vallenatomanizales@gmial.com>
2024-07-22 23:09:40 -0500

    feat: ui, button component

---
+++ ui/src/components/Button/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="ss-button">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss">
+.ss-button {
+  background-color: $c-primary;
+  min-height: $input-height;
+  color: $c-white;
+  border: none;
+  padding: $s2 $s4;
+  border-radius: $border-radius-base;
+  cursor: pointer;
+  transition: $transition-base;
+
+  &:hover {
+    background-color: $c-primary-hover;
+  }
+}
+</style>
