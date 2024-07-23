6ed4568e5e165a7c730c533f8d86d250ac840bf6
yamid-granda <vallenatomanizales@gmial.com>
2024-07-22 23:08:09 -0500

    feat: ui, card component

---
+++ ui/src/components/Card/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="ss-card">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.ss-card {
+  border-radius: $border-radius-base;
+  padding: $input-gutter;
+  background-color: $c-white;
+  border: $border-base;
+}
+
+@include tablet {
+  .ss-card {
+    padding: $s8;
+  }
+}
+</style>
