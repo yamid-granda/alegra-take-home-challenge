bf109dcb4d740d4d2de7631e6926d3b93cf83a91
yamid-granda <vallenatomanizales@gmial.com>
2024-07-22 23:08:51 -0500

    feat: ui, loader component

---
+++ ui/src/components/Loader/Loader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="ss-loader" />
+</template>
+
+<style lang="scss">
+.ss-loader {
+  width: 48px;
+  height: 48px;
+  border: 10px solid $c-white;
+  border-radius: 50%;
+  position: relative;
+  transform: rotate(45deg);
+  box-sizing: border-box;
+}
+
+.ss-loader::before {
+  content: "";
+  position: absolute;
+  box-sizing: border-box;
+  inset: -10px;
+  border-radius: 50%;
+  border: 10px solid $c-primary;
+  animation: prixClipFix 1.2s infinite linear;
+}
+
+@keyframes prixClipFix {
+  0%   {clip-path:polygon(50% 50%,0 0,0 0,0 0,0 0,0 0)}
+  25%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 0,100% 0,100% 0)}
+  50%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,100% 100%,100% 100%)}
+  75%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,0 100%,0 100%)}
+  100% {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,0 100%,0 0)}
+}
+</style>
