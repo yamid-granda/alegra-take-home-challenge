2c9919238f3df3705a9fec06d7ca1cac0d1ea4aa
yamid-granda <vallenatomanizales@gmial.com>
2024-07-23 17:43:26 -0500

    fix: ui, remove unused test util

--- ui/src/test-utils/findOptionByText/index.ts
+++
@@ -1,5 +0,0 @@
-import { screen } from '@testing-library/react'
-
-export async function findOptionByText(name: string): Promise<HTMLOptionElement> {
-  return screen.findByRole('option', { name })
-}
