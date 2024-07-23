a3e6e6344205566a5a6ced5984e22c031a2ebad4
yamid-granda <vallenatomanizales@gmial.com>
2024-07-22 23:09:11 -0500

    feat: ui, progress bar component

---
+++ ui/src/components/ProgressBar/ProgressBar.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import type { IProgressBarProps } from './types'
+
+const props = defineProps<IProgressBarProps>()
+
+const max = 20
+
+const progress = computed<number>(() => {
+  const progress = (props.value / max) * 100
+  return progress > 100 ? 100 : progress
+})
+</script>
+
+<template>
+  <div class="ss-progress-bar">
+    <div class="ss-progress-bar__info">
+      <span>{{ value }} puntos</span>
+      <span>de {{ max }}</span>
+    </div>
+
+    <div class="ss-progress-bar__bar">
+      <div
+        class="ss-progress-bar__progress"
+        :style="{ width: `${progress}%` }"
+      />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.ss-progress-bar__bar {
+  position: relative;
+  width: 100%;
+  border: $border-base;
+  height: $s4;
+  border-radius: $border-radius-base;
+  overflow: hidden;
+}
+
+.ss-progress-bar__info {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: $s1;
+}
+
+.ss-progress-bar__progress {
+  transition: $transition-base;
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  width: 0%;
+  background-color: $c-primary;
+}
+</style>
